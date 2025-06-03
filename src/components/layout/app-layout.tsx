a7801812b2b6d3d797c48ecc4b509362372baad8
Antonio Diaz S. <sadian88@gmail.com>
2025-06-03 19:23:31 +0000

    quiero que el main principal del sitio ocupe todo el alto y ancho de pantalla, sin montarse encima del menu

--- src/components/layout/app-layout.tsx
+++ src/components/layout/app-layout.tsx
@@ -1,3 +1,4 @@
+
 import type { ReactNode } from 'react';
 import { Header } from './header';
 import { Footer } from './footer';
@@ -10,10 +11,11 @@ export function AppLayout({ children }: AppLayoutProps) {
   return (
     <div className="flex flex-col min-h-screen">
       <Header />
-      <main className="flex-grow container mx-auto px-4 py-8">
+      <main className="flex-grow">
         {children}
       </main>
       <Footer />
     </div>
   );
 }
+
